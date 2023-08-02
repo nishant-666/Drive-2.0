d1ea62f475cb263036f7c73e7cdd5c9dc1bacfc2
Nishant Kumar <nishants440@gmail.com>
2023-08-02 13:37:28 +0530

    Fixes

--- src/firebaseConfig.tsx
+++ src/firebaseConfig.tsx
@@ -2,15 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDX4Wrz1VCtEUhXfe9bkro9V4J_Wj4Tu5g",
-  authDomain: "firedrive-218d5.firebaseapp.com",
-  projectId: "firedrive-218d5",
-  storageBucket: "firedrive-218d5.appspot.com",
-  messagingSenderId: "660142855896",
-  appId: "1:660142855896:web:a967fa4c02e0f091d629ad",
+const config = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
 };
 
-export const app = initializeApp(firebaseConfig);
+export const app = initializeApp(config);
 export const storage = getStorage(app);
 export const database = getFirestore(app);
